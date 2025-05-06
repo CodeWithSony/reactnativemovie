6cc66b393230016da08702414f2402f28b53d5d2
codewithsony <sahu.priyanka2122@gmail.com>
2025-05-06 13:30:33 +0530

    video added

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,58 @@
 import SearchBar from "@/components/SearchBar";
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
+import { Video } from "expo-av";
 import { useRouter } from "expo-router";
-import { Image, ScrollView, View } from "react-native";
+import { useEffect, useState } from "react";
+import {
+  ActivityIndicator,
+  FlatList,
+  Image,
+  ScrollView,
+  Text,
+  View,
+} from "react-native";
+
+type VideoItem = {
+  _id: string;
+  videoUrl: string;
+};
 
 export default function Index() {
   // npx expo start --clear
   const router = useRouter();
+  const [videos, setVideos] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchVideos = async () => {
+      try {
+        const response = await fetch(
+          "https://video-streaming-phi.vercel.app/api/get-videos"
+        );
+        const data = await response.json();
+        setVideos(data);
+      } catch (error) {
+        console.error("Error fetching videos:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchVideos();
+  }, []);
+  const renderItem = ({ item }: { item: VideoItem }) => (
+    <View style={{ flex: 1, margin: 5 }}>
+      <Video
+        source={{ uri: item.videoUrl }}
+        useNativeControls
+        resizeMode="cover"
+        style={{ width: "100%", height: 200 }}
+      />
+      <Text style={{ color: "white", marginTop: 5 }}>{item._id}</Text>
+    </View>
+  );
+
   return (
     <View className="flex-1 bg-primary">
       <Image source={images.bg} className="absolute w-full z-0" />
@@ -17,14 +63,36 @@ export default function Index() {
       >
         <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
 
-        <View className="flex-1 mt-5">
-          <SearchBar
-            onPress={() => {
-              router.push("/search");
-            }}
-            placeholder="Search for a movie"
+        {loading ? (
+          <ActivityIndicator
+            size="large"
+            color="#0000ff"
+            className="mt-10 self-center"
           />
-        </View>
+        ) : (
+          <View className="flex-1 mt-5">
+            <SearchBar
+              onPress={() => {
+                router.push("/search");
+              }}
+              placeholder="Search for a movie"
+            />
+
+            <>
+              <Text className="text-lg text-white font-bold mt-5 mb-3">
+                Latest Movies
+              </Text>
+
+              <FlatList
+                data={videos}
+                keyExtractor={(item) => item._id}
+                renderItem={renderItem}
+                contentContainerStyle={{ padding: 10 }}
+                numColumns={2}
+              />
+            </>
+          </View>
+        )}
       </ScrollView>
     </View>
   );
---
+++ services/api.ts
@@ -0,0 +1,5 @@
+export const TMDB_COFIG = {
+  // BASE_URL = 'https://video-streaming-phi.vercel.app/api/get-videos'
+
+  BASE_URL: "https://video-streaming-phi.vercel.app",
+};
