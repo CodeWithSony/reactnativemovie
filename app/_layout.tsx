5b9f745a8b1e85540d676c7cf48874ad397b840f
codewithsony <sahu.priyanka2122@gmail.com>
2025-05-06 10:33:01 +0530

    first commit

---
+++ app/_layout.tsx
@@ -0,0 +1,22 @@
+import { Stack } from "expo-router";
+import "./globals.css";
+export default function RootLayout() {
+  return (
+    <Stack>
+      {/* here headetshown false is hiding group routes */}
+      <Stack.Screen
+        name="(tabs)"
+        options={{
+          headerShown: false,
+        }}
+      />
+
+      <Stack.Screen
+        name="movie/[id]"
+        options={{
+          headerShown: false,
+        }}
+      />
+    </Stack>
+  );
+}
